a840f4c4ab4bfa1b48306486219fc38488e0ca27
Steve Melo <steve.melo@rewardops.com>
2021-05-05 11:16:29 -0400

    Added second lable. and check against number of reviewers

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,17 @@ if [[ -z "$addLabel" ]]; then
   exit 1
 fi
 
+addLabel2=$ADD_LABEL2
+if [[ -n "$LABEL_NAME" ]]; then
+  echo "Warning: Plase define the ADD_LABEL2 variable instead of the deprecated LABEL_NAME2."
+  addLabel2=$LABEL_NAME2
+fi
+
+if [[ -z "$addLabel2" ]]; then
+  echo "Set the ADD_LABEL2 or the LABEL_NAME2 env variable."
+  exit 1
+fi
+
 URI="https://api.github.com"
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
@@ -37,22 +48,26 @@ number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
 label_when_approved() {
   # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
-  body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
-  reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state} | @base64')
+  body1=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
+  reviews=$(echo "$body1" | jq --raw-output '.[] | {state: .state} | @base64')
 
+  body2=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/requested_reviewers?per_page=100")
+  requested_reviewers=$(echo "body2" | jq '.users | length')
+  
   approvals=0
 
   for r in $reviews; do
     review="$(echo "$r" | base64 -d)"
     rState=$(echo "$review" | jq --raw-output '.state')
+  
 
     if [[ "$rState" == "APPROVED" ]]; then
       approvals=$((approvals+1))
     fi
 
-    echo "${approvals}/${APPROVALS} approvals"
+    echo "${approvals}/${requested_reviewers} approvals"
 
-    if [[ "$approvals" -ge "$APPROVALS" ]]; then
+    if [[ "$approvals" -ge "$requested_reviewers" ]]; then
       echo "Labeling pull request"
 
       curl -sSL \
@@ -60,7 +75,7 @@ label_when_approved() {
         -H "${API_HEADER}" \
         -X POST \
         -H "Content-Type: application/json" \
-        -d "{\"labels\":[\"${addLabel}\"]}" \
+        -d "{\"labels\":[\"${addLabel}\",\"${addLabel2}\"]}" \
         "${URI}/repos/${GITHUB_REPOSITORY}/issues/${number}/labels"
 
       if [[ -n "$REMOVE_LABEL" ]]; then
